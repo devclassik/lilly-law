1abcf5ddf77e0ef7a9e72eab788f6b2875746f72
Opemipo <alomajaopemipo8@gmail.com>
2024-08-29 00:51:16 +0100

    link

--- src/pages/dashboard/comingSoon.jsx
+++ src/pages/dashboard/comingSoon.jsx
@@ -7,7 +7,7 @@ export const ComingSoon = () => {
         <p className="text-lg mb-6">
           We're working hard to bring you something amazing! Stay tuned.
         </p>
-        <p className="text-lg">
+        <p className="text-base">
           In the meantime, please kindly forward all your documents to
           <a href="mailto:info@vccchartteredattoneychamber.com" className="text-blue-600 underline ml-2">
             info@vccchartteredattoneychamber.com
